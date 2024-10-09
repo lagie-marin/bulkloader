43877b25ebf1bd3f6af01ed3bed31647f1fa087b
lagie-marin <marin.lagie@epitech.eu>
2024-10-09 12:50:50 +0200

    [UPDATE]name project

--- index.d.ts
+++ index.d.ts
@@ -4,6 +4,6 @@
  * @param dirPath - Le chemin vers le répertoire contenant les modules.
  * @param args - Les arguments à passer aux modules lors de leur appel.
  */
-declare function bulkLoader(dirPath: string, args: any[]): void;
+declare function bulkloader(dirPath: string, args: any[]): void;
 
-export = bulkLoader;
+export = bulkloader;
